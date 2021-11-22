6cad2da4b2bd15ba52b6698e568e13e01d46c6be
nzhdnva <natali06zhd@gmail.com>
2021-11-22 11:47:24 +0300

    Initial commit

---
+++ classif.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from imblearn.over_sampling import SMOTE
+
+pd.options.display.max_columns = None
+pd.options.display.width = None
