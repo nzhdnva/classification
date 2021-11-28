48ba161e1931a5d51b68b86d6efb983c02770b0f
nzhdnva <natali06zhd@gmail.com>
2021-11-28 11:38:56 +0300

    init

--- classif.py
+++ classif.py
@@ -6,3 +6,29 @@ from imblearn.over_sampling import SMOTE
 
 pd.options.display.max_columns = None
 pd.options.display.width = None
+
+df = pd.read_csv('https://github.com/nzhdnva/ML-For-Beginners/blob/main/4-Classification/data/cuisines.csv?raw=true')
+
+thai_df = df[(df.cuisine == "thai")]
+japanese_df = df[(df.cuisine == "japanese")]
+chinese_df = df[(df.cuisine == "chinese")]
+indian_df = df[(df.cuisine == "indian")]
+korean_df = df[(df.cuisine == "korean")]
+
+def create_ingredient_df(df):
+    ingredient_df = df.T.drop(['cuisine','Unnamed: 0']).sum(axis=1).to_frame('value')
+    ingredient_df = ingredient_df[(ingredient_df.T != 0).any()]
+    ingredient_df = ingredient_df.sort_values(by='value', ascending=False, inplace=False)
+    return ingredient_df
+
+feature_df = df.drop(['cuisine','Unnamed: 0','rice','garlic','ginger'], axis=1)
+labels_df = df.cuisine #.unique()
+
+oversample = SMOTE()
+transformed_feature_df, transformed_label_df = oversample.fit_resample(feature_df, labels_df)
+
+#print(f'new label count: {transformed_label_df.value_counts()}')
+#print(f'old label count: {df.cuisine.value_counts()}')
+
+transformed_df = pd.concat([transformed_label_df,transformed_feature_df],axis=1, join='outer')
+transformed_df.to_csv("cleaned_cuisines.csv")
